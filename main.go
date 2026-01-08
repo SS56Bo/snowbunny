166f0b5f802fdd727c787efb18e8b9fab51350e3
SS56Bo <sohamsarkar345@gmail.com>
2026-01-08 23:51:16 +0530

    Minor updates

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
+
+type Config struct {
+	ListenAddress string
+}
+type Server struct {
+	Config
+	listen net.Listener
+}
+
+func CreateNewServer(cfg Config) *Server {
+	return &Server{
+		Config: cfg,
+	}
+}
 
 func main() {
 	fmt.Println("Snowbunny - SMTP built in Golang")
