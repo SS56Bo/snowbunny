330a10f878568561d379295e58117339929503fc
SS56Bo <sohamsarkar345@gmail.com>
2026-01-09 23:58:13 +0530

    Added error logger

--- main.go
+++ main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 )
 
 type Config struct {
-	ListenAddress string  // Port or something else
+	ListenAddress string // Port or something else
 }
 type Server struct {
 	Config
 	listen net.Listener
 }
 
+func errorLogger(err error) {
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+}
+
 func CreateNewServer(cfg Config) *Server {
-	if (len(cfg.ListenAddress)) == 0 {
+	if len(cfg.ListenAddress) == 0 {
 		cfg.ListenAddress = ":7777"
 	}
 	return &Server{
@@ -24,9 +31,22 @@ func CreateNewServer(cfg Config) *Server {
 
 func (s *Server) StartServer() error {
 	listen, err := net.Listen("tcp", s.ListenAddress)
-	if err := nil {
-		log.Printf("ERROR: %v", err)
-	}
+	errorLogger(err)
+
+	s.listen = listen
+	s.acceptLoop()
+	return nil
+}
+
+func (s *Server) acceptLoop() {
+	conn, err := s.listen.Accept()
+
+	s.handleConn(conn)
+	errorLogger(err)
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+
 }
 
 func main() {
